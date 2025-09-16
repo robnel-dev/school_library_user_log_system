92dcb47719b221ceeb5d4df3baccc8dd1ae2a789
Rendel <rj.cuizon0804@gmail.com>
2025-09-16 13:05:48 +0800

    Final output

--- public/footer.php
+++ public/footer.php
@@ -8,7 +8,7 @@
     font-weight:300; 
     border-top:1px solid rgba(255,255,255,0.1);
 ">
-    <p style="margin:2px 0;">&copy; <?php echo date('Y'); ?> SPCBA Library User Log</p>
+    <p style="margin:2px 0;">&copy; <?php echo date('Y'); ?> SPCBA Library User Log System</p>
     <p style="margin:2px 0;">
         Developed by 
         <a href="https://www.instagram.com/rbnl_robledo?igsh=cWgwZjhobzE1b3Rs" style="color:#67d46b; text-decoration:none; font-weight:500;">John Ronnel Z. Robledo</a> &amp; 
